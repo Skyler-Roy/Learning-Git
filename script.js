18377d7fd99a0d297fb8fb77ccb4332c125e83ef
Abhishek Singh Rathore <vanixdexter108@gmail.com>
2025-09-13 11:50:39 +0530

    Revert "mt/.MERGE_MSG.swp"
    This reverts commit 96356adec94de29b89ee2695f9adba2d351bce1a, reversing
    changes made to 828bf96bca71f4fdf350552099728f9490b64f8c.

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ console.log("Hi this is a change in script.js");
 console.log("Learning Git and Github is fun!");
 const getRandomNumber = Math.floor(Math.random()*101);
 console.log(getRandomNumber);
-console.log(getRandomNumber + 100);
